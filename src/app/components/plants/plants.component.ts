4266ae979492edfc52bd1a04042fe09c89df5a40
Nicolas Salvai <nicolas-salvai@hotmail.com>
2021-08-15 16:16:22 -0300

    filtering for 15 min intervals

--- src/app/components/plants/plants.component.ts
+++ src/app/components/plants/plants.component.ts
@@ -19,38 +19,42 @@ export class PlantsComponent implements OnInit {
   constructor(private plantsService: PlantsService) { }
 
   ngOnInit(): void { 
-    let aux_plants: Plant[] = [];
     this.plantsService.getPlants()
       .subscribe((data) => {
         for(let i = 0; i < data.length; i++){
           this.plants.push(new Plant((data[i].payload.doc.data() as Plant).plant_id,
-            (data[i].payload.doc.data() as Plant).start_time,
+            new Date((data[i].payload.doc.data() as Plant).start_time),
             (data[i].payload.doc.data() as Plant).page,
             (data[i].payload.doc.data() as Plant).owner_id,
             (data[i].payload.doc.data() as Plant).id))
         }
 
+        this.plants = this.plants.filter((element) => {           
+          let now = new Date();
+          if((now.getHours()*60 + now.getMinutes()) > (element.start_time.getHours()*60 + element.start_time.getMinutes() + 15) || (now.getHours()*60 + now.getMinutes()) < (element.start_time.getHours()*60 + element.start_time.getMinutes() - 15))
+            return false
+          return true;
+        })
+
         this.plants = this.plants.sort((a, b) => {
-          if(parseInt(a.start_time.substring(11,13)) > parseInt(b.start_time.substring(11,13))){
+          if(a.start_time.getHours() > b.start_time.getHours()){
             return 1;
-          } else if (parseInt(a.start_time.substring(11,13)) < parseInt(b.start_time.substring(11,13))){
+          } else if (a.start_time.getHours() < b.start_time.getHours()){
             return -1;
           }
-          if(parseInt(a.start_time.substring(14,16)) > parseInt(b.start_time.substring(14,16))){
+          if(a.start_time.getMinutes() > b.start_time.getMinutes()){
             return 1;
-          } else if (parseInt(a.start_time.substring(14,16)) < parseInt(b.start_time.substring(14,16))){
+          } else if (a.start_time.getMinutes() < b.start_time.getMinutes()){
             return -1;
           }
-          if(parseInt(a.start_time.substring(17,19)) > parseInt(b.start_time.substring(17,19))){
+          if(a.start_time.getSeconds() > b.start_time.getSeconds()){
             return 1;
-          } else if (parseInt(a.start_time.substring(17,19)) < parseInt(b.start_time.substring(17,19))){
+          } else if (a.start_time.getSeconds() < b.start_time.getSeconds()){
             return -1;
           }
           return 1;
         });
       })
-    console.log(this.plants.length)
-    console.log(this.plants)
    }
 
   public addJson(jsonInput: any) {
--- src/app/models/plant.ts
+++ src/app/models/plant.ts
@@ -1,10 +1,10 @@
 export class Plant {
     plant_id: number;
-    start_time: string;
+    start_time: Date;
     page: number;
     owner_id: string;
     id: string;
-    constructor(plant_id: number, start_time: string, page: number, owner_id: string, id: string){
+    constructor(plant_id: number, start_time: Date, page: number, owner_id: string, id: string){
         this.plant_id = plant_id;
         this.start_time = start_time;
         this.page = page;
